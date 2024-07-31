e873ec18c833226ef52c7c3c7e161d46b26b2028
DanielDT12 <daniel.digitaletalenter@gmail.com>
2024-07-31 14:15:20 +0200

    Satt opp async api call og funksjon for display

--- index.js
+++ index.js
@@ -1,5 +1,23 @@
 async function getAdvice() {
-    try {
-        let re
-    }
+	try {
+		const response = await fetch("https://api.adviceslip.com/advice");
+		const adviceData = await response.json();
+		if (!response.ok) {
+			throw new Error("Network did not respond with 200");
+		}
+
+		displayAdvice(adviceData);
+	} catch (error) {
+		console.error("Error fetching data from API", error);
+	}
 }
+
+function displayAdvice(adviceData) {
+	const adviceNumber = document.querySelector("#js-advice-number");
+	const adviceText = document.querySelector("#js-advice-text");
+
+	adviceNumber.textContent = `Advice #${adviceData.slip.id}`;
+	adviceText.textContent = adviceData.slip.advice;
+}
+
+getAdvice();
